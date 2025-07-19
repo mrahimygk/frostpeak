d08111862709d704b3e73d29fb427c32f505fc1b
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2025-07-19 17:21:31 +0330

    moving camera logic out

--- core/src/main/kotlin/com/mojtabarahimy/frostpeak/FrostPeakGame.kt
+++ core/src/main/kotlin/com/mojtabarahimy/frostpeak/FrostPeakGame.kt
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.utils.ScreenUtils
 import com.badlogic.gdx.utils.viewport.FitViewport
+import com.mojtabarahimy.frostpeak.controller.CameraController
 import com.mojtabarahimy.frostpeak.entities.Player
 import com.mojtabarahimy.frostpeak.input.PlayerInputProcessor
 import com.mojtabarahimy.frostpeak.util.Constants
@@ -22,6 +23,7 @@ class FrostPeakGame : ApplicationAdapter() {
     private val targetCamPos = Vector2()
 
     lateinit var playerInputProcessor: PlayerInputProcessor
+    lateinit var cameraController: CameraController
 
     override fun create() {
         batch = SpriteBatch()
@@ -32,6 +34,8 @@ class FrostPeakGame : ApplicationAdapter() {
 
         viewport.apply()
         camera.setToOrtho(false, viewport.worldWidth, viewport.worldHeight)
+
+        cameraController = CameraController(camera, viewport)
 
         val texture = Texture("player.png") // Put this image in core/assets
         player = Player(texture)
@@ -44,10 +48,10 @@ class FrostPeakGame : ApplicationAdapter() {
         camera.update()
 
         //Gdx.app.log("Frostpeak", "Texture loaded: ${playerTexture.width}x${playerTexture.height}")
-/*
-        playerX = 100f
-        playerY = 100f
-*/
+        /*
+                playerX = 100f
+                playerY = 100f
+        */
         playerInputProcessor = PlayerInputProcessor { dx, dy ->
             player.move(dx, dy)
         }
@@ -60,33 +64,7 @@ class FrostPeakGame : ApplicationAdapter() {
 
         playerInputProcessor.update(delta, Constants.speed)
 
-        val safeMarginX = viewport.worldWidth / 3f
-        val safeMarginY = viewport.worldHeight / 3f
-
-        val camLeft = camera.position.x - viewport.worldWidth / 2f + safeMarginX
-        val camRight = camera.position.x + viewport.worldWidth / 2f - safeMarginX
-        val camBottom = camera.position.y - viewport.worldHeight / 2f + safeMarginY
-        val camTop = camera.position.y + viewport.worldHeight / 2f - safeMarginY
-
-        targetCamPos.set(camera.position.x, camera.position.y)
-
-        if (player.x < camLeft) {
-            targetCamPos.x = player.x - safeMarginX + viewport.worldWidth / 2f
-        } else if (player.x > camRight) {
-            targetCamPos.x = player.x + safeMarginX - viewport.worldWidth / 2f
-        }
-
-        if (player.y < camBottom) {
-            targetCamPos.y = player.y - safeMarginY + viewport.worldHeight / 2f
-        } else if (player.y > camTop) {
-            targetCamPos.y = player.y + safeMarginY - viewport.worldHeight / 2f
-        }
-
-        val lerpFactor = 5f * Gdx.graphics.deltaTime  // Adjust this for smoothness
-
-        camera.position.x += (targetCamPos.x - camera.position.x) * lerpFactor
-        camera.position.y += (targetCamPos.y - camera.position.y) * lerpFactor
-        camera.update()
+        cameraController.update(delta, player.getCameraFocusX(), player.getCameraFocusY())
 
         ScreenUtils.clear(0.5f, 0.8f, 1f, 1f) // sky blue background
         batch.projectionMatrix = camera.combined
---
+++ core/src/main/kotlin/com/mojtabarahimy/frostpeak/controller/CameraController.kt
@@ -0,0 +1,44 @@
+package com.mojtabarahimy.frostpeak.controller
+
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.utils.viewport.Viewport
+
+class CameraController (
+    private val camera: OrthographicCamera,
+    private val viewport: Viewport,
+    private val marginFraction: Float = 1f / 3f,
+    private val lerpSpeed: Float = 5f
+){
+    private val targetPos = Vector2(camera.position.x, camera.position.y)
+
+    fun update(delta: Float, focusX: Float, focusY: Float){
+        val safeMarginX = viewport.worldWidth * marginFraction
+        val safeMarginY = viewport.worldHeight * marginFraction
+
+        val camLeft = camera.position.x - viewport.worldWidth / 2f + safeMarginX
+        val camRight = camera.position.x + viewport.worldWidth / 2f - safeMarginX
+        val camBottom = camera.position.y - viewport.worldHeight / 2f + safeMarginY
+        val camTop = camera.position.y + viewport.worldHeight / 2f - safeMarginY
+
+        // Determine target position
+        targetPos.set(camera.position.x, camera.position.y)
+
+        if (focusX < camLeft) {
+            targetPos.x = focusX - safeMarginX + viewport.worldWidth / 2f
+        } else if (focusX > camRight) {
+            targetPos.x = focusX + safeMarginX - viewport.worldWidth / 2f
+        }
+
+        if (focusY < camBottom) {
+            targetPos.y = focusY - safeMarginY + viewport.worldHeight / 2f
+        } else if (focusY > camTop) {
+            targetPos.y = focusY + safeMarginY - viewport.worldHeight / 2f
+        }
+
+        // Lerp the camera to the target
+        camera.position.x += (targetPos.x - camera.position.x) * lerpSpeed * delta
+        camera.position.y += (targetPos.y - camera.position.y) * lerpSpeed * delta
+        camera.update()
+    }
+}
