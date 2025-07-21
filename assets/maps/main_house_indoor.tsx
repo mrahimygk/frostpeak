df098c6e6fb8942c5747c5435f7152a3bb8e51a4
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2025-07-21 10:54:53 +0330

    adding house indoor maps

---
+++ assets/maps/main_house_indoor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="main_house_indoor" tilewidth="4" tileheight="4" tilecount="7020" columns="90">
+ <image source="main_house_indoor.png" width="360" height="314"/>
+</tileset>
---
+++ assets/maps/persian_rug.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="persian_rug" tilewidth="4" tileheight="4" tilecount="900" columns="36">
+ <image source="persian_rug.png" width="144" height="101"/>
+</tileset>
