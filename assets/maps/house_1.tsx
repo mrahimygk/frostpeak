634f05a41663c5e422d7f5be8abd907e70f1e40f
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2025-08-06 13:55:37 +0330

    Adding a village house for an npc

---
+++ assets/maps/house_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="house_1" tilewidth="4" tileheight="4" tilecount="650" columns="26">
+ <image source="house/house_1.png" width="107" height="100"/>
+</tileset>
