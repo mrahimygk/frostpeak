0c7ea4d81df939d464a2f933996cebcd03ea9c5f
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2025-08-12 11:11:59 +0330

    bug fix: faulty function

--- core/src/main/kotlin/com/mojtabarahimy/frostpeak/entities/tools/ChangeableStateImpl.kt
+++ core/src/main/kotlin/com/mojtabarahimy/frostpeak/entities/tools/ChangeableStateImpl.kt
@@ -4,6 +4,6 @@ class ChangeableStateImpl : ChangeableState {
     override var stateChanged: Boolean = false
 
     override fun changeState(newState: Boolean) {
-        stateChanged = true
+        stateChanged = newState
     }
 }
